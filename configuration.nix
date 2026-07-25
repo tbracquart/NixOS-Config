fe9c114379e0ce6104d42a1e4703310ce01fb699
Thibaut Bracquart <202062783+tbracquart@users.noreply.github.com>
2026-07-25 23:12:57 +0200

    Noctalia

--- configuration.nix
+++ configuration.nix
@@ -1,7 +1,24 @@
 { config, pkgs, ... }:
 
+let
+  noctalia-src = builtins.fetchTarball {
+    url = "https://github.com/noctalia-dev/noctalia/archive/cachix.tar.gz";
+    sha256 = "003bdgvig2nsbr5688diwz08yr9i5h0hmfwm1r4z4gxn3smw9bwa";
+  };
+in
 {
+  nix.settings = {
+    substituters = [ "https://noctalia.cachix.org" ];
+    trusted-public-keys = [ "noctalia.cachix.org-1:Q+Yc4k8bqOUb5hbZB30DoePrspQ2qrm/WcB1MsOa3wE=" ];
+  };
+
+  # 👇 OPTIONNEL MAIS RECOMMANDÉ pour Hyprland + Noctalia
+  programs.hyprland.withUWSM = true;
+
+  # ... le reste de ta config
+
   imports = [
+    "${noctalia-src}/nix/nixos-module.nix"
     ./hardware-configuration.nix
     ./modules/system.nix
     ./modules/services.nix
@@ -11,14 +28,13 @@
     ./kdeconnect-openfirewall.nix
   ];
 
-  disabledModules = [ "programs/kdeconnect.nix" ];
-
-
-  virtualisation.vmVariant = {
-    # Définit un mot de passe temporaire pour votre utilisateur, uniquement dans la VM
-    users.users.thibaut.initialPassword = "test";
+  programs.noctalia = {
+    enable = true;
+    # Optionnel : configurer le service systemd
+    # systemd.enable = true;
   };
 
+  disabledModules = [ "programs/kdeconnect.nix" ];
 
   system.stateVersion = "26.05";
 }
