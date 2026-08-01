5c45f5be1b8fffe6a9b4a5730f60efb0293fc7fa
Thibaut Bracquart <202062783+tbracquart@users.noreply.github.com>
2026-08-01 20:18:16 +0200

    feat: migration complète vers une architecture Flake unifiée avec Home Manager

--- flake.nix
+++ flake.nix
@@ -1,26 +1,44 @@
 {
-  description = "Flake NixOS pour ZenBook-13";
+  description = "Configuration NixOS + Home Manager pour ZenBook 13";
 
   inputs = {
-    # Ton système sur NixOS 26.05
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-26.05";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
 
-    # Noctalia avec son propre nixpkgs (unstable)
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    # Noctalia Shell (branche cachix)
     noctalia = {
-      url = "github:noctalia-dev/noctalia";
+      url = "github:noctalia-dev/noctalia/cachix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    # FreesmLauncher
+    freesmlauncher = {
+      url = "github:FreesmTeam/FreesmLauncher/develop";
+      inputs.nixpkgs.follows = "nixpkgs";
     };
   };
 
-  outputs = { self, nixpkgs, noctalia, ... }@inputs: {
-    nixosConfigurations = {
-      "ZenBook-13" = nixpkgs.lib.nixosSystem {
-        system = "x86_64-linux";
-        modules = [
-          noctalia.nixosModules.default  # <-- Correction ici !
-          ./hardware-configuration.nix
-          ./configuration.nix
-        ];
-      };
+  outputs = { self, nixpkgs, home-manager, noctalia, freesmlauncher, ... }@inputs: {
+    nixosConfigurations.ZenBook-13 = nixpkgs.lib.nixosSystem {
+      system = "x86_64-linux";
+      specialArgs = { inherit inputs; };
+      modules = [
+        ./hardware-configuration.nix  # <-- On l'ajoute ici !
+        ./configuration.nix
+
+        # Module NixOS de Home Manager
+        home-manager.nixosModules.home-manager
+        {
+          home-manager.useGlobalPkgs = true;
+          home-manager.useUserPackages = true;
+          home-manager.extraSpecialArgs = { inherit inputs; };
+          home-manager.users.thibaut = import ./home.nix;
+        }
+      ];
     };
   };
 }
