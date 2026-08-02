86fdfda807a7ec6344bb57b6ddeb47a0d5a29917
Thibaut Bracquart <202062783+tbracquart@users.noreply.github.com>
2026-08-02 15:57:45 +0200

    cible home-manager release-26.05 pour matcher nixpkgs stable

--- flake.nix
+++ flake.nix
@@ -17,7 +17,7 @@
     nixpkgs.url = "github:nixos/nixpkgs/nixos-26.05";
 
     home-manager = {
-      url = "github:nix-community/home-manager";
+      url = "github:nix-community/home-manager/release-26.05";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
@@ -54,20 +54,6 @@
       modules = [
         ./hosts/V145-15AST/configuration.nix
         # Pas de Home Manager ici : machine partagée, pas de config perso à toi dessus.
-      ];
-    };
-
-    nixosConfigurations.ZenBook-13-vmtest = nixpkgs.lib.nixosSystem {
-      specialArgs = { inherit inputs; };
-      modules = [
-        ./hosts/ZenBook-13-vmtest/configuration.nix
-        home-manager.nixosModules.home-manager
-        {
-          home-manager.useGlobalPkgs = true;
-          home-manager.useUserPackages = true;
-          home-manager.extraSpecialArgs = { inherit inputs; };
-          home-manager.users.thibaut = import ./hosts/ZenBook-13-vmtest/home.nix;
-        }
       ];
     };
   };
