4eda8dc2efc6efe8ea8fde8f82a6927ffe4336d7
Thibaut Bracquart <202062783+tbracquart@users.noreply.github.com>
2026-08-19 12:59:36 +0200

    feat(thunar)

--- common/hyprland.lua
+++ common/hyprland.lua
@@ -16,7 +16,7 @@ hl.monitor({
 ---- MY PROGRAMS ----
 ---------------------
 local terminal    = "alacritty"
-local fileManager = "dolphin"
+local fileManager = "thunar"
 local menu        = ""
 
 -------------------
