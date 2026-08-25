b482c20afbd4ebfb0a83ff1900c31fcb515eb1e1
Thibaut Bracquart <202062783+tbracquart@users.noreply.github.com>
2026-08-25 14:07:55 +0200

    refactor: isoler le démarrage propre au V145
    Déplace la configuration systemd-boot et du noyau du V145 dans un fichier dédié au host. Cette configuration dépend directement de la machine et ne doit pas être placée dans common ni dans un profil générique.

---
+++ hosts/V145-15AST/boot.nix
@@ -0,0 +1,10 @@
+{ pkgs, ... }:
+
+{
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+  boot.loader.timeout = 1;
+
+  boot.kernelPackages = pkgs.linuxPackages_latest;
+  boot.kernelParams = [ ];
+}
