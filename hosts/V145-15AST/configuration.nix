5bc30657a538df05693550633a57c80a7db6e8d4
Thibaut Bracquart <202062783+tbracquart@users.noreply.github.com>
2026-08-25 14:07:44 +0200

    refactor: réduire le host V145 à son assemblage
    Retire de hosts/V145-15AST/configuration.nix les réglages déjà représentés par les couches communes et le profil Plasma. Le host conserve uniquement ses particularités locales, notamment la désactivation du TPM et les choix qui restent à traiter spécifiquement pour cette machine. Cette étape rapproche sa structure de celle du ZenBook sans modifier volontairement son matériel.

--- hosts/V145-15AST/configuration.nix
+++ hosts/V145-15AST/configuration.nix
@@ -1,85 +1,28 @@
-{ config, pkgs, ... }:
+{ ... }:
 
 {
   imports = [
     ./hardware-configuration.nix
     ./variables.nix
     ./users.nix
+
+    ../../common/system/nix.nix
+    ../../common/system/networking.nix
+    ../../common/system/locale.nix
+    ../../common/system/services.nix
+    ../../profiles/plasma.nix
   ];
-
-  nix.settings.experimental-features = [ "nix-command" "flakes" ];
-
-  boot.loader.systemd-boot.enable = true;
-  boot.loader.efi.canTouchEfiVariables = true;
-  boot.loader.timeout = 1;
-
-  boot.kernelPackages = pkgs.linuxPackages_latest;
-  boot.kernelParams = [ ];
 
   systemd.tpm2.enable = false;
   boot.initrd.systemd.tpm2.enable = false;
 
-  services.avahi = {
-    enable = true;
-    nssmdns4 = true;
-    publish = { enable = true; addresses = true; };
-  };
-
-  networking.networkmanager.enable = true;
-
-  services.xserver.enable = true;
-  services.displayManager.sddm.enable = true;
-  services.desktopManager.plasma6.enable = true;
-
-  services.xserver.xkb = {
-    layout = "fr";
-    variant = "";
-  };
-
-  services.printing.enable = true;
-
   services.pulseaudio.enable = false;
   security.rtkit.enable = true;
-  services.pipewire = {
-    enable = true;
-    alsa.enable = true;
-    alsa.support32Bit = true;
-    pulse.enable = true;
-  };
 
   programs.firefox.enable = true;
-
   nixpkgs.config.allowUnfree = true;
 
-  environment.systemPackages = with pkgs; [
-    fastfetch
-  ];
-
-  programs = {
-    fish = {
-      enable = true;
-      interactiveShellInit = ''
-        fastfetch
-
-        function clr
-          clear
-          fastfetch
-        end
-      '';
-    };
-
-    kdeconnect.enable = true;
-    git = {
-      enable = true;
-      config = {
-        user.name = "Thibaut Bracquart";
-        user.email = "202062783+tbracquart@users.noreply.github.com";
-      };
-    };
-  };
-
-  services.openssh.enable = true;
-  services.flatpak.enable = true;
+  environment.systemPackages = [ ];
 
   system.stateVersion = "26.05";
 }
