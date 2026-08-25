521f7d08e986e8c90a60a6676de9e9cfb039e625
Thibaut Bracquart <202062783+tbracquart@users.noreply.github.com>
2026-08-25 14:16:11 +0200

    refactor: isoler les services propres au ZenBook
    Conserve sur le ZenBook les services greetd et geoclue2 qui étaient auparavant cachés dans common/system/services.nix. Ces services sont liés à l’environnement Noctalia du ZenBook et ne doivent pas être imposés au V145.

---
+++ hosts/ZenBook-13/services.nix
@@ -0,0 +1,12 @@
+{ ... }:
+
+{
+  services.greetd.enable = true;
+
+  services.geoclue2 = {
+    enable = true;
+    geoProviderUrl = "https://api.beacondb.net/v1/geolocate";
+  };
+
+  location.provider = "geoclue2";
+}
