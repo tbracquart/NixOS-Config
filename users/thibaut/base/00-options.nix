4469f34f8b37f239658653bc0bd6b785f2c7643a
Thibaut Bracquart <202062783+tbracquart@users.noreply.github.com>
2026-08-25 02:06:08 +0200

    Structurer le profil utilisateur de Thibaut

---
+++ users/thibaut/base/00-options.nix
@@ -0,0 +1,9 @@
+{ config, lib, ... }:
+
+{
+  options.my.flakePath = lib.mkOption {
+    type = lib.types.str;
+    default = "${config.home.homeDirectory}/nixos-config";
+    description = "Chemin absolu vers le dépôt nixos-config";
+  };
+}
