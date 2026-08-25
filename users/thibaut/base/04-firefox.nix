4bec8b5f599403511483bb9c9c466c6091ff50c5
Thibaut Bracquart <202062783+tbracquart@users.noreply.github.com>
2026-08-25 18:43:29 +0200

    refactor: simplifier la configuration Firefox
    Firefox ne doit fournir que l'installation du navigateur. Les extensions,
    le profil, le chemin de profil et l'intégration Pywalfox étaient des
    personnalisations utilisateur qui ne sont pas nécessaires à la
    reproductibilité du système et doivent être recréées manuellement après
    une réinstallation.

--- users/thibaut/base/04-firefox.nix
+++ users/thibaut/base/04-firefox.nix
@@ -1,16 +1,5 @@
-{ pkgs, inputs, ... }:
+{ ... }:
 
 {
-  programs.firefox = {
-    enable = true;
-    nativeMessagingHosts = [ pkgs.pywalfox-native ];
-    profiles.thibaut = {
-      id = 0;
-      isDefault = true;
-      path = "2jto0bxf.default";
-      extensions.packages = with inputs.firefox-addons.packages.${pkgs.stdenv.hostPlatform.system}; [
-        pywalfox
-      ];
-    };
-  };
+  programs.firefox.enable = true;
 }
