f10f91d053a6764de5f8e7fe8233a98663336711
Thibaut Bracquart <202062783+tbracquart@users.noreply.github.com>
2026-08-26 18:23:42 +0200

    fix: restaurer le clavier global dans le profil Plasma

--- profiles/system/plasma.nix
+++ profiles/system/plasma.nix
@@ -2,6 +2,10 @@
 
 {
   services.xserver.enable = true;
+  services.xserver.xkb = {
+    layout = "global";
+    variant = "";
+  };
   services.displayManager.sddm.enable = true;
   services.desktopManager.plasma6.enable = true;
 }
