0a45c20f26730098064153cd685f43e6e5eff709
Thibaut Bracquart <202062783+tbracquart@users.noreply.github.com>
2026-08-26 18:06:36 +0200

    fix: supprimer le conflit de clavier du profil Plasma

--- profiles/system/plasma.nix
+++ profiles/system/plasma.nix
@@ -4,9 +4,4 @@
   services.xserver.enable = true;
   services.displayManager.sddm.enable = true;
   services.desktopManager.plasma6.enable = true;
-
-  services.xserver.xkb = {
-    layout = "fr";
-    variant = "";
-  };
 }
