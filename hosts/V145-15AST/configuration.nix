bda80435b0bb8ff54351a3117a5ab5779a50c974
Thibaut Bracquart <202062783+tbracquart@users.noreply.github.com>
2026-08-27 00:59:07 +0200

    refactor: piloter les modules depuis le host V145

--- hosts/V145-15AST/configuration.nix
+++ hosts/V145-15AST/configuration.nix
@@ -19,12 +19,14 @@
     ../../common/users/thibaut.nix
     ../../common/system/packages.nix
 
-    ../../modules/desktop/plasma.nix
-    ../../modules/authentication/howdy.nix
+    ../../modules
     ../../profiles/laptop.nix
   ];
 
   networking.hostName = "V145-15AST";
+
+  my.authentication.howdy.enable = true;
+  my.desktop.plasma.enable = true;
 
   system.stateVersion = "26.05";
 }
