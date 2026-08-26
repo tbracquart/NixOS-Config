be31dc7a390c213ebaeb948d4fc2bd895b132f0c
Thibaut Bracquart <202062783+tbracquart@users.noreply.github.com>
2026-08-27 00:57:59 +0200

    refactor: exposer Hyprland via une option personnalisée

--- modules/desktop/hyprland.nix
+++ modules/desktop/hyprland.nix
@@ -1,33 +1,41 @@
-{ inputs, pkgs, ... }:
+{ config, inputs, pkgs, lib, ... }:
 
+let
+  cfg = config.my.desktop.hyprland;
+in
 {
-  imports = [
-    inputs.noctalia-greeter.nixosModules.default
-  ];
+  options.my.desktop.hyprland.enable = lib.mkEnableOption
+    "l'environnement de bureau Hyprland";
 
-  services.libinput.enable = true;
-
-  programs.hyprland.enable = true;
-
-  xdg.portal = {
-    enable = true;
-    extraPortals = [
-      pkgs.xdg-desktop-portal-hyprland
-      pkgs.xdg-desktop-portal-gtk
+  config = lib.mkIf cfg.enable {
+    imports = [
+      inputs.noctalia-greeter.nixosModules.default
     ];
-    config.common.default = "gtk";
-  };
 
-  programs.noctalia = {
-    enable = true;
-    recommendedServices.enable = true;
-  };
+    services.libinput.enable = true;
 
-  programs.noctalia-greeter = {
-    enable = true;
-    settings = {
-      keyboard.layout = "global";
-      output.scale = 1;
+    programs.hyprland.enable = true;
+
+    xdg.portal = {
+      enable = true;
+      extraPortals = [
+        pkgs.xdg-desktop-portal-hyprland
+        pkgs.xdg-desktop-portal-gtk
+      ];
+      config.common.default = "gtk";
+    };
+
+    programs.noctalia = {
+      enable = true;
+      recommendedServices.enable = true;
+    };
+
+    programs.noctalia-greeter = {
+      enable = true;
+      settings = {
+        keyboard.layout = "global";
+        output.scale = 1;
+      };
     };
   };
 }
