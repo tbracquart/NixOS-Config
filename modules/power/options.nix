243796485bac6997a8cf5f452437db77788090e5
Thibaut Bracquart <202062783+tbracquart@users.noreply.github.com>
2026-08-27 00:59:20 +0200

    refactor: remplacer le module batterie par son module unique

--- modules/power/options.nix
+++
@@ -1,9 +0,0 @@
-{ lib, ... }:
-
-{
-  options.my.power.batteryChargeLimit = lib.mkOption {
-    type = lib.types.nullOr (lib.types.ints.between 0 100);
-    default = null;
-    description = "Limite de charge de la batterie en pourcentage.";
-  };
-}
