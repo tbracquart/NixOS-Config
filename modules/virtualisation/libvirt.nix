ec2dfe656f21ca089be33245f0a98660dced4e71
Thibaut Bracquart <202062783+tbracquart@users.noreply.github.com>
2026-08-27 16:34:54 +0200

    refactor(virtualisation): manage libvirt user access in module
    Add an optional user setting to the libvirt module so access to the libvirtd group is configured alongside libvirt itself. This removes the host-specific implementation detail from users.nix while keeping the target user explicit.

--- modules/virtualisation/libvirt.nix
+++ modules/virtualisation/libvirt.nix
@@ -4,11 +4,22 @@ let
   cfg = config.my.virtualisation.libvirt;
 in
 {
-  options.my.virtualisation.libvirt.enable = lib.mkEnableOption
-    "la virtualisation avec libvirt";
+  options.my.virtualisation.libvirt = {
+    enable = lib.mkEnableOption "la virtualisation avec libvirt";
+
+    user = lib.mkOption {
+      type = lib.types.nullOr lib.types.str;
+      default = null;
+      description = "Utilisateur à ajouter au groupe libvirtd.";
+    };
+  };
 
   config = lib.mkIf cfg.enable {
     virtualisation.libvirtd.enable = true;
     programs.virt-manager.enable = true;
+
+    users.users.${cfg.user}.extraGroups = lib.mkIf (cfg.user != null) [
+      "libvirtd"
+    ];
   };
 }
