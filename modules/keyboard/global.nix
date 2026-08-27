b1aafba7b31973b72b0ecdd597fb9f135344fbfc
Thibaut Bracquart <202062783+tbracquart@users.noreply.github.com>
2026-08-27 15:52:42 +0200

    feat: add keyboard configuration API

---
+++ modules/keyboard/global.nix
@@ -0,0 +1,30 @@
+{ config, lib, pkgs, ... }:
+
+let
+  cfg = config.my.keyboard.global;
+  xkbDir = pkgs.runCommand "azerty-global-xkb" { } ''
+    mkdir -p "$out"
+    cp -rL --no-preserve=mode "${pkgs.xkeyboard_config}/etc/X11/xkb/." "$out/"
+    cp ${../../common/system/azerty_global} "$out/symbols/global"
+  '';
+in
+{
+  options.my.keyboard.global.enable = lib.mkEnableOption
+    "le clavier AZERTY Global";
+
+  config = lib.mkIf cfg.enable {
+    services.xserver.xkb = {
+      dir = xkbDir;
+      layout = "global";
+      variant = "";
+    };
+
+    console.useXkbConfig = true;
+
+    environment.sessionVariables = {
+      XKB_CONFIG_ROOT = xkbDir;
+      XKB_DEFAULT_LAYOUT = "global";
+      XKB_DEFAULT_VARIANT = "";
+    };
+  };
+}
