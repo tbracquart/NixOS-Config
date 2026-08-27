04fd92989bfeefa2df77a183f16eedc347a99db4
Thibaut Bracquart <202062783+tbracquart@users.noreply.github.com>
2026-08-27 14:53:47 +0200

    refactor: add common configuration entrypoint

---
+++ common/default.nix
@@ -0,0 +1,14 @@
+{
+  imports = [
+    ./system/nix.nix
+    ./system/networking.nix
+    ./system/locale.nix
+    ./system/keyboard.nix
+    ./system/services.nix
+    ./system/security.nix
+    ./system/shell.nix
+    ./system/programs.nix
+    ./system/packages.nix
+    ./users/thibaut.nix
+  ];
+}
