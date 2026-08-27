91037ed3f8c2b292e42844538de6ee4fc00c7c8d
Thibaut Bracquart <202062783+tbracquart@users.noreply.github.com>
2026-08-27 15:54:32 +0200

    refactor: make V145 host declarative

--- hosts/V145-15AST/configuration.nix
+++ hosts/V145-15AST/configuration.nix
@@ -1,22 +1,6 @@
 {
-  ...
-}:
-
-{
-  imports = [
-    ./hardware-configuration.nix
-    ./users.nix
-    ./boot.nix
-
-    ../../common
-    ../../modules
-    ../../profiles/laptop.nix
-  ];
-
-  networking.hostName = "V145-15AST";
+  my.profile = "laptop";
 
   my.authentication.howdy.enable = true;
   my.desktop.plasma.enable = true;
-
-  system.stateVersion = "26.05";
 }
