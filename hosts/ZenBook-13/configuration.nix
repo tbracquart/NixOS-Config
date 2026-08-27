272f8468be1df6983311e987c4fd8a08429511a2
Thibaut Bracquart <202062783+tbracquart@users.noreply.github.com>
2026-08-27 15:54:25 +0200

    refactor: make ZenBook host declarative

--- hosts/ZenBook-13/configuration.nix
+++ hosts/ZenBook-13/configuration.nix
@@ -1,43 +1,12 @@
 {
-  pkgs,
-  ...
-}:
-
-{
-  imports = [
-    ./hardware-configuration.nix
-    ./users.nix
-    ./boot.nix
-
-    ../../common
-    ../../modules
-    ../../profiles/laptop.nix
-  ];
-
-  networking.hostName = "ZenBook-13";
+  my.profile = "laptop";
 
   my.authentication.howdy.ir.enable = true;
   my.compatibility.nix-ld.enable = true;
   my.desktop.hyprland.enable = true;
+  my.firmware.redistributable.enable = true;
+  my.graphics.intel.enable = true;
   my.location.geoclue2.enable = true;
   my.power.batteryChargeLimit = 80;
   my.virtualisation.libvirt.enable = true;
-
-  hardware.graphics = {
-    enable = true;
-    extraPackages = with pkgs; [
-      intel-media-driver
-      vpl-gpu-rt
-    ];
-  };
-
-  environment.sessionVariables.LIBVA_DRIVER_NAME = "iHD";
-  hardware.enableRedistributableFirmware = true;
-
-  hardware.bluetooth = {
-    enable = true;
-    powerOnBoot = true;
-  };
-
-  system.stateVersion = "26.05";
 }
