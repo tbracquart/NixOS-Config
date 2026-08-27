bcbf1d36a024fa176066ac58f7141ca84f35223b
Thibaut Bracquart <202062783+tbracquart@users.noreply.github.com>
2026-08-27 16:34:06 +0200

    refactor(host): make keyboard layout explicit

--- hosts/ZenBook-13/configuration.nix
+++ hosts/ZenBook-13/configuration.nix
@@ -5,6 +5,7 @@
   my.compatibility.nix-ld.enable = true;
   my.desktop.hyprland.enable = true;
   my.graphics.intel.enable = true;
+  my.keyboard.global.enable = true;
   my.location.geoclue2.enable = true;
   my.power.batteryChargeLimit = 80;
   my.virtualisation.libvirt.enable = true;
