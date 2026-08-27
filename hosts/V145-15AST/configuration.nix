e4e94357e706d2aae958a89dddad6cb6bc46a88f
Thibaut Bracquart <202062783+tbracquart@users.noreply.github.com>
2026-08-27 14:54:11 +0200

    refactor: simplify V145 module imports

--- hosts/V145-15AST/configuration.nix
+++ hosts/V145-15AST/configuration.nix
@@ -8,19 +8,8 @@
     ./users.nix
     ./boot.nix
 
-    ../../common/system/nix.nix
-    ../../common/system/networking.nix
-    ../../common/system/locale.nix
-    ../../common/system/keyboard.nix
-    ../../common/system/services.nix
-    ../../common/system/security.nix
-    ../../common/system/shell.nix
-    ../../common/system/programs.nix
-    ../../common/users/thibaut.nix
-    ../../common/system/packages.nix
-
-    ../../modules/authentication
-    ../../modules/desktop
+    ../../common
+    ../../modules
     ../../profiles/laptop.nix
   ];
 
