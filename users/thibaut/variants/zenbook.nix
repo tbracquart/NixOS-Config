d589590fe20429f7184a59d50e18c3fdff659b0e
Thibaut Bracquart <202062783+tbracquart@users.noreply.github.com>
2026-09-03 11:06:33 +0200

    chore(desktop): rename mutable-configs to dotfiles
    Renomme le dossier contenant les configurations Hyprland (Lua) et Noctalia (TOML) pour adopter une nomenclature standard. Mise à jour des chemins mkOutOfStoreSymlink associés dans le fichier de configuration de l'hôte ZenBook.

--- users/thibaut/variants/zenbook.nix
+++ users/thibaut/variants/zenbook.nix
@@ -3,11 +3,11 @@
 {
   home.file.".local/state/noctalia/settings.toml".source =
     config.lib.file.mkOutOfStoreSymlink
-      "${config.my.flakePath}/users/thibaut/hyprland/mutable-configs/noctalia-settings.toml";
+      "${config.my.flakePath}/users/thibaut/dotfiles/noctalia-settings.toml";
 
   home.file.".config/hypr/init.lua".source =
     config.lib.file.mkOutOfStoreSymlink
-      "${config.my.flakePath}/users/thibaut/hyprland/mutable-configs/hyprland.lua";
+      "${config.my.flakePath}/users/thibaut/dotfiles/hyprland.lua";
 
   wayland.windowManager.hyprland = {
     enable = true;
