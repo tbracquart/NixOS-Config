eb4f0cb3800bde147491120c13c0c52041c8ea31
Thibaut Bracquart <202062783+tbracquart@users.noreply.github.com>
2026-09-05 13:59:10 +0200

    chore: normaliser l'ordre des imports communs

--- common/default.nix
+++ common/default.nix
@@ -1,13 +1,13 @@
 {
   imports = [
-    ./system/nix.nix
-    ./system/networking.nix
     ./system/locale.nix
-    ./system/services.nix
-    ./system/security.nix
-    ./system/shell.nix
-    ./system/programs.nix
+    ./system/networking.nix
+    ./system/nix.nix
     ./system/packages.nix
+    ./system/programs.nix
+    ./system/security.nix
+    ./system/services.nix
+    ./system/shell.nix
     ./users/thibaut.nix
   ];
 }
