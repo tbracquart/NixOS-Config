fb2e641f69a5ea2e08ff88a7c1526cac68d2561b
Thibaut Bracquart <202062783+tbracquart@users.noreply.github.com>
2026-09-05 19:01:48 +0200

    refactor: activate laptop profile through selector

--- profiles/laptop.nix
+++ profiles/laptop.nix
@@ -1,6 +1,8 @@
-{ lib, ... }:
+{ config, lib, ... }:
 
 {
-  my.bluetooth.enable = lib.mkDefault true;
-  my.power.batteryChargeLimit = lib.mkDefault 80;
+  config = lib.mkIf (config.my.profile == "laptop") {
+    my.bluetooth.enable = lib.mkDefault true;
+    my.power.batteryChargeLimit = lib.mkDefault 80;
+  };
 }
