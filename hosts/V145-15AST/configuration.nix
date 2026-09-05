6cd926bfeab54e29f2dce4a2ff694d12ce097b7a
Thibaut Bracquart <202062783+tbracquart@users.noreply.github.com>
2026-09-05 02:24:48 +0200

    test: remove temporary flake impact package

--- hosts/V145-15AST/configuration.nix
+++ hosts/V145-15AST/configuration.nix
@@ -1,13 +1,7 @@
-{ inputs, pkgs, ... }:
-
 {
   my.profile = "laptop";
 
   my.authentication.howdy.enable = true;
   my.desktop.plasma.enable = true;
   my.flatpak.enable = true;
-
-  environment.systemPackages = [
-    (pkgs.writeText "ci-flake-impact-test" (builtins.toString inputs.nix-cachyos-kernel))
-  ];
 }
