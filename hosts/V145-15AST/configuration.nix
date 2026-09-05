22445d849815ce099068d0771bb94d36a0c9eaa0
Thibaut Bracquart <202062783+tbracquart@users.noreply.github.com>
2026-09-05 18:05:28 +0200

    refactor: make V145 configuration the host module

--- hosts/V145-15AST/configuration.nix
+++ hosts/V145-15AST/configuration.nix
@@ -1,4 +1,38 @@
+{ inputs, ... }:
+
 {
+  imports = [
+    ./hardware-configuration.nix
+    ./boot.nix
+    ./users.nix
+    ../../common
+    ../../modules
+    ../../profiles
+    inputs.home-manager.nixosModules.default
+    inputs.sops-nix.nixosModules.sops
+  ];
+
+  networking.hostName = "V145-15AST";
+  system.stateVersion = "26.05";
+
+  home-manager = {
+    useGlobalPkgs = true;
+    useUserPackages = true;
+    extraSpecialArgs = { inherit inputs; };
+
+    users.thibaut = {
+      imports = [
+        ../../users/thibaut/base
+      ];
+    };
+
+    users.quentin = {
+      imports = [
+        ../../users/quentin/base.nix
+      ];
+    };
+  };
+
   my.profile = "laptop";
 
   my.authentication.howdy.enable = true;
